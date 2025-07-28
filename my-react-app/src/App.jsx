49525c4bec08433dbc313066a844568a60e10c3c
Rana2411cs010549 <2411cs010549@mallareddyuniversity.ac.in>
2025-07-28 13:44:56 +0530

    fix: correct spelling of 'Chennai' to 'Chenna' in travel data

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -29,7 +29,7 @@ const Culturist = () => {
   const travelData = {
    TamilNadu: {
   cities: {
-    Chennai: {
+    Chenna: {
       Winter: {
         festivals: ['Pongal', 'Music Season Festival', 'Christmas'],
         food: ['Sambar', 'Filter Coffee', 'Dosa', 'Murukku'],
